caa5e4e6ebed3add388a8ce42dd90ece1bdd8a91
Ömer Dumlupınar <omerdumlupinar0@gmail.com>
2024-12-15 20:06:48 +0300

    .

--- Services/Catalog/MultiShop.Catalog/obj/Debug/net7.0/MultiShop.Catalog.AssemblyInfo.cs
+++ Services/Catalog/MultiShop.Catalog/obj/Debug/net7.0/MultiShop.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6515ae05e9b9010be09c60dc32486ba6d89a0e6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bf70b9399d0902142d9452fc3755e5ef9260fc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
